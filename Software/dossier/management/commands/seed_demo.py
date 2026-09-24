ff1f3a99cc0e7c0656e0ce4206bd1f5877ef8bcd
Ewoud Bouttelisier <ewoud.bouttelisier@gmail.com>
2026-09-24 13:15:49 +0200

    New file structure

--- Software/dossier/management/commands/seed_demo.py
+++ Software/dossier/management/commands/seed_demo.py
@@ -1,5 +1,5 @@
 from django.core.management.base import BaseCommand
-from EducatiefEPD.dossier.models import Assignment, Case, Module, Patient
+from EducatiefEPD.Software.dossier.models import Assignment, Case, Module, Patient
 
 
 class Command(BaseCommand):
